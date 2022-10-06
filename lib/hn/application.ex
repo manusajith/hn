36d19b52f01e168af2b75f42075274555a38fbd7
Manu S Ajith <neo@codingarena.in>
2022-10-06 01:39:28 -0500

    Persist stories in ETS
    Signed-off-by: Manu S Ajith <neo@codingarena.in>

--- lib/hn/application.ex
+++ lib/hn/application.ex
@@ -4,12 +4,19 @@ defmodule HN.Application do
   use Application
 
   @impl true
+  @spec start(any, any) :: {:error, any} | {:ok, pid}
   def start(_type, _args) do
+    initialise_storage()
+
     children = [
       {Finch, name: HN.Finch}
     ]
 
     opts = [strategy: :one_for_one, name: HN.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp initialise_storage do
+    :ets.new(:stories, [:named_table, :public])
   end
 end
--- lib/hn/data/client.ex
+++ lib/hn/data/client.ex
@@ -8,8 +8,6 @@ defmodule HN.Data.Client do
   alias HN.Data.HttpClient
   alias HN.Storage.Story
 
-  require Logger
-
   @doc """
   Fetch top stories from HN API and save it to ETS Table.
   """
@@ -17,7 +15,16 @@ defmodule HN.Data.Client do
   def fetch_top_stories() do
     top_story_ids = fetch_top_story_ids()
 
-    stream = Task.async_stream(top_story_ids, &fetch_story/1, ordered: false, timeout: 30_000)
+    stream =
+      Task.async_stream(
+        top_story_ids,
+        fn id ->
+          id |> fetch_story |> save_story
+        end,
+        ordered: false,
+        max_concurrency: 100,
+        timeout: 30_000
+      )
 
     Stream.run(stream)
   end
@@ -29,8 +36,11 @@ defmodule HN.Data.Client do
 
   defp fetch_story(id) do
     url = @base_url <> "/item/#{id}.json"
-    story = HttpClient.get(url)
+    HttpClient.get(url)
+  end
+
+  defp save_story(story) do
     story = struct(%Story{}, story)
-    Logger.debug(story)
+    :ets.insert(:stories, {story.id, story})
   end
 end
