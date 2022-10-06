35b533b61cedd46cd3fc29e5363abc35d6d535c1
Manu S Ajith <neo@codingarena.in>
2022-10-06 02:14:35 -0500

    Implement pagination for /stories endpoint
    Signed-off-by: Manu S Ajith <neo@codingarena.in>

--- lib/hn/api/router.ex
+++ lib/hn/api/router.ex
@@ -19,7 +19,43 @@ defmodule HN.API.Router do
   end
 
   get "/stories" do
-    {stories, _cont} = Repo.list(:stories, 10)
+    {stories, _continuation} =
+      case conn.query_string do
+        "" ->
+          fetch_stories("")
+
+        query ->
+          "page=" <> page = query
+          page = String.to_integer(page)
+          fetch_stories(page)
+      end
+
     send_resp(conn, 200, Jason.encode!(stories))
+  end
+
+  match _ do
+    send_resp(conn, 404, "404")
+  end
+
+  defp fetch_stories(page) when page == "" do
+    Repo.list(:stories, 10)
+  end
+
+  defp fetch_stories(page) when page == 1 do
+    Repo.list(:stories, 10)
+  end
+
+  defp fetch_stories(page) when page > 1 do
+    {_stories, cont} = Repo.list(:stories, 10)
+    fetch_stories(page - 1, cont)
+  end
+
+  defp fetch_stories(page, cont) when page == 0 do
+    Repo.list(cont)
+  end
+
+  defp fetch_stories(page, cont) when page > 0 do
+    {_stories, cont} = Repo.list(cont)
+    fetch_stories(page - 1, cont)
   end
 end
